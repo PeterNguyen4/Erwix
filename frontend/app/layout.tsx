0388814b9822d334761c842a1cff13530476234f
PeterNguyen4 <peternguyen644@gmail.com>
2026-07-24 16:31:00 -0700

    bug: wrap clerk tag for refresh

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider afterSignOutUrl="/login">
       <html lang="en">
         <head>
           <link rel="icon" href="/entro.svg" />
