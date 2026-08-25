67d31a32650c2e2912d7c660ecf3df15f43308c9
PeterNguyen4 <peternguyen644@gmail.com>
2026-08-25 16:48:08 -0700

    fix: change default journal time for mobile to be daily

---
+++ frontend/lib/useIsMobile.ts
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function useIsMobile(): boolean {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const mql = window.matchMedia("(max-width: 767px)");
+    const update = () => setIsMobile(mql.matches);
+    update();
+    mql.addEventListener("change", update);
+    return () => mql.removeEventListener("change", update);
+  }, []);
+  return isMobile;
+}
